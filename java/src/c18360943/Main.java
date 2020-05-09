38cdf3cc0d0a7aa4a37705ebe30453565aa823f4
seandelaney43 <45828379+seandelaney43@users.noreply.github.com>
2020-05-09 15:05:55 +0100

    Add files via upload
    Set up Main and Visuals in the same format as the example code and visual exception. Added the song I am going to be using to the data folder.

---
+++ java/src/c18360943/Main.java
@@ -0,0 +1,18 @@
+package c18360943;
+
+
+public class Main { //OOP Project
+
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		Visuals visual = new Visuals();
+		main.startApp();
+	}
+	
+	public void startApp() {
+		String[] a = {"MAIN"};
+		processing.core.PApplet.runSketch(a, new Visuals());
+	}
+
+}
---
+++ java/src/c18360943/VisualException.java
@@ -0,0 +1,19 @@
+package c18360943;
+
+public class VisualException extends Throwable {
+	
+	
+private static final long serialVersionUID = 1L;
+    
+    private String message;
+    
+    public VisualException(String message)
+    {
+        this.message = message;
+    }
+
+    public String toString()
+    {
+        return message;
+    }
+}
