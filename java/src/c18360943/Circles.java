8f7dcd6d4b8083c817af5b5c3a2f6e261b89f23a
seandelaney43 <45828379+seandelaney43@users.noreply.github.com>
2020-05-16 21:17:20 +0100

    Update Circles.java
    Changed my approach to displaying shapes. Using amplitude to change the size/color of the circles on-screen and also showing an amplitude map at the bottom of the screen which can be moved to the top by pressing 't'. Pressing the space bar will pause/play the music.

--- java/src/c18360943/Circles.java
+++ java/src/c18360943/Circles.java
@@ -5,81 +5,109 @@ import java.util.LinkedList;
 import processing.core.PImage;
 
 public class Circles extends Visuals {
-	private int screenWidth = 1024; //screenWidth and screenHeight are set to the dimensions  of the photo that I am using for my background
-	private int screenHeight = 775;
-	int circleCount = 10; //number of circles to be displayed
-	Circle[] circles = new Circle[circleCount];
+	int screenWidth = 1024; //screenWidth and screenHeight are set to the dimensions  of the photo that I am using for my background
+	int screenHeight = 775;
 	private PImage backgroundImage;
 	private String bGround = "Realm.jpg";
+	private float hue;
+	LinkedList<Float> history = new LinkedList();
+	boolean top=false;
 	
 	public void settings() {
 		size(screenWidth, screenHeight);
 	}
 	
-	public void keyPressed() //Functional keys for playing and pausing the audio
-    {
-        if (key == ' ')
-        {
-            getAudioPlayer().play();
-            
-        }
-        if(key == 'p') {
-        	getAudioPlayer().pause();
-        }
-    }
-	
 	public void setup() {
 		colorMode(HSB);
 		backgroundImage = loadImage(bGround);
 		startMinim();
-		loadAudio("TheRealm.mp3");
+		loadAudio("heroplanet.mp3");
 		getAudioPlayer().play();
-		startListening();
 	}
 	
 	public void draw() {
 		calculateAverageAmplitude();
-		LinkedList<Float> list = new LinkedList();
-		for(int i = 0; i < 360; i++) {
-			list.add(getSmoothenedAmplitude());
-		}
 		calculateFrequencyBands();
 		background(backgroundImage);
 		colorMode(HSB);
-		stroke(255);
+		history.add(getSmoothenedAmplitude());
+		hue = map(getSmoothenedAmplitude() * 1000, -400, 300, 0 ,255);
+		stroke(hue, 255, 255);
 		noFill();
 		translate(screenWidth/2 , screenHeight/2);
-		beginShape();
-		for(int i = 0; i<360; i++) {
-			float r = map(list.get(i) , 0 , 1, 10, 400);
-			float x= r * cos(i) * 2;
-			float y = r * sin(i) * 2;
+		Visualiser();
+		createCircles();
+	}
 	
+	
+	public void keyPressed() //Functional keys for playing and pausing the audio
+    {
+        if (key == ' ')
+        {
+            if(getAudioPlayer().isPlaying()) {
+            	getAudioPlayer().pause();
+            }
+            else {
+            	getAudioPlayer().play();
+            }
+        }
+        if(key == 't') {
+        	if(top==false) {
+        		top=true;
+        	}
+        	else {
+        		top=false;
+        	}
+        }
+    }
+	
+	void Visualiser() {
+		//Amplitude visualizer at the bottom/top of the screen
+		beginShape();
+		for(int i=0;i<history.size();i++) {
+			float y = map(history.get(i), 0, 1, screenHeight/2, 0);
+			if(top==false) {
+				vertex(i,y);
+			}
+			else {
+				vertex(-i,-y);
+			}
+		}
+		if(history.size()==screenWidth/2) {
+			history.clear();
+		}
+		endShape();
+		
+				//Amplitude visualizer mirror
+		beginShape();
+		for(int i=0;i<history.size();i++) {
+			float y = map(history.get(i), 0, 1, screenHeight/2, 0);
+			if(top==false) {
+				vertex((-i),y);
+			}
+			else {
+				vertex(i,-y);
+			}
+		}
+				endShape();
+	}
+	
+	void createCircles() {
+		beginShape();
+		for(int i = 0; i<360;i++) {
+			float radius = map(getSmoothenedAmplitude(), 0, 1, 10, 500);
+			float x = radius * cos(i);
+			float y = radius * sin(i);
 			vertex(x,y);
 		}
 		endShape();
 		beginShape();
-		for(int i = 0; i<360; i++) {
-				float k = map(getSmoothenedAmplitude(), 0 , 1 , 10, 500);
-				float t = k * cos(i); 
-				float j = k * sin(i);
-			
-			vertex(t,j);
+		for(int i = 0; i<360 ; i++){
+			float radius = map(getSmoothenedAmplitude() , 0 , 1 , 10 , 100);
+			float x = radius * cos(i) * 4;
+			float y = radius * sin(i) * 4;
+			vertex(x,y);
 		}
 		endShape();
-		beginShape();
-		for(int i = 0 ; i<360; i++) {
-			float r = map(getSmoothenedAmplitude(), 0, 1, 10, 100);
-			float x = r * cos(i) * 4;
-			float y = r * sin(i) * 4; 
-			
-			vertex (x,y);
-		}
-		endShape();
-		beginShape();
-		stroke(250);
-		ellipse(0 ,0 ,screenWidth , getSmoothenedAmplitude() * 2000);
-		endShape();
-			
 	}
 }
